3e5cac7b8aa078909d75365c02bb38b06bfb0214
vanquan25 <minhquan22012@gmail.com>
2022-05-13 10:35:12 +0700

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular_we16304';
+  name = 'quanlvph1794';
+  class = 'we16304';
+
+  teachers = [
+    {
+      id: 1,
+      name: "GV1",
+      age: 60,
+      gender: 1,
+      avatar: "https://picsum.photos/100",
+      status: 0
+    },
+    {
+      id: 2,
+      name: "GV2",
+      age: 25,
+      gender: 0,
+      avatar: "https://picsum.photos/100",
+      status: 1
+    },
+    {
+      id: 3,
+      name: "GV3",
+      age: 25,
+      gender: 0,
+      avatar: "https://picsum.photos/100",
+      status: 1
+    },
+    {
+      id: 4,
+      name: "GV4",
+      age: 50,
+      gender: 1,
+      avatar: "https://picsum.photos/100",
+      status: 0
+    },
+    {
+      id: 5,
+      name: "GV5",
+      age: 25,
+      gender: 1,
+      avatar: "https://picsum.photos/100",
+      status: 1
+    }
+  ]
+
+  studentName= 'Quanlv'
+  studentId= 'PH17984'
 }
