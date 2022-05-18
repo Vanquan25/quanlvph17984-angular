dc7517ba94cc695313d7061168b703a2817208f7
vanquan25 <minhquan22012@gmail.com>
2022-05-18 09:38:51 +0700

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -55,4 +55,63 @@ export class AppComponent {
 
   studentName= 'Quanlv'
   studentId= 'PH17984'
+
+  schoolName = '';
+
+  clickH1(){
+    console.log('Da click vao H1');
+    this.schoolName = 'Poly';
+  }
+
+  showStatus = true;
+  changeTableStatus(){
+    this.showStatus = !this.showStatus;
+  }
+  inputValue = "update";
+  changeInput(event:any){
+    this.inputValue = event.target.value;
+  }
+
+
+  //dinh nghia ham nhan gia tri tu cac input
+  inputValues = {
+    name: '',
+    age: '',
+    gender: '0',
+    avatar: '',
+    status: '1'
+    
+  };
+  // onInputName(event: any, info: string){
+  //   this.inputValues['name'] = event.target.value;
+  // }
+  // onInputAge(event: any, info: string){
+  //   this.inputValues.age = event.target.value;
+  // }
+
+  // key: 'name' | 'age' -> key chỉ dc là giá trị 'name' hoặc 'age'
+  onInput(event: any, key: 'name' | 'age' | 'gender' | 'avatar' | 'status') {
+    this.inputValues[key] = event.target.value;
+  }
+
+  //Sự kiện click vào nút Submit
+  onSubmit() {
+    this.teachers.push({
+      ...this.inputValues,
+      age: +this.inputValues.age,   // đưa age từ chuỗi input về số
+      // bổ sung các thuộc tính còn đang thiếu
+      gender: +this.inputValues.gender,
+      status: +this.inputValues.status,
+      id: this.teachers.length + 1
+    });
+    // cập nhật lại giá trị cho input ở form:
+    // [value]="this.inputValues.name"
+    this.inputValues = {
+      name: '',
+      age: '',
+      gender: '0',
+      avatar: '',
+      status: '1'
+    }
+  }
 }
